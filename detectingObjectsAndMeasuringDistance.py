477437ab74f4bcb2fe16c5cc54700e4799eb02f6
Ali Baran Kaya <79401738+kaya-baran@users.noreply.github.com>
2021-02-26 16:28:09 +0300

    Update detectingObjectsAndMeasuringDistance.py

--- detectingObjectsAndMeasuringDistance.py
+++ detectingObjectsAndMeasuringDistance.py
@@ -27,9 +27,6 @@ def on_click(x, y, button, pressed):
                     if newX_Angle == scanned_angle:
                         print(actual_distance)
                     
-        # print (f'Mouse clicked at ({x}, {y}) with {button}')
-
-
 def setupLidar():
     lidar = PyRPlidar()
 
@@ -51,9 +48,6 @@ def stopLidar(lidar):
     lidar.set_motor_pwm(0)
     lidar.disconnect()
 
-
-#def simple_scan(lidar):
-
 parser = argparse.ArgumentParser(description="Locate objects in a live camera stream using an object detection DNN.", 
                                 formatter_class=argparse.RawTextHelpFormatter, epilog=jetson.inference.detectNet.Usage() +
                                 jetson.utils.videoSource.Usage() + jetson.utils.videoOutput.Usage() + jetson.utils.logUsage())
@@ -74,36 +68,23 @@ except:
     parser.print_help()
     sys.exit(0)
 
-# load the object detection network
 net = jetson.inference.detectNet(opt.network, sys.argv, opt.threshold)
 
-# create video sources & outputs
 input = jetson.utils.videoSource(opt.input_URI, argv=sys.argv)
 output = jetson.utils.videoOutput(opt.output_URI, argv=sys.argv+is_headless)
 tempX = 0
 tempY = 0
 slidar = setupLidar()
 
-# process frames until the user exits
 with Listener(on_click=on_click) as listener:
     startLidar(lidar)   
     while True:
-		# capture the next image
         img = input.Capture()
 
-		# detect objects in the image (with overlay)
         detections = net.Detect(img, overlay=opt.overlay)
-		
-		# print the detections
-		#print("detected {:d} objects in image".format(len(detections)))
 
-		#for detection in detections:
-		#	print(detection)
-
-		# render the image
         output.Render(img)
 
-		# update the title bar
         output.SetStatus("{:s} | Network {:.0f} FPS".format(opt.network, net.GetNetworkFPS()))
 		
         if(tempX != coordX or tempY != coordY):
@@ -117,11 +98,6 @@ with Listener(on_click=on_click) as listener:
                 print("{0} .class => {1},{2},{3},{4} Mouse => {5},{6}".format(i.ClassID,i.Left,i.Top,i.Right,i.Bottom,coordX,coordY))
                 break
 
-
-		# print out performance info
-		#net.PrintProfilerTimes()
-
-		# exit on input/output EOS
         if not input.IsStreaming() or not output.IsStreaming():
             stopLidar(lidar)
             break
